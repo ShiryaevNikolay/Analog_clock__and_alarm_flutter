e46549602509320b0efa9de96772c02f46c36c80
ShiryaevNikolay <deendeeps@list.ru>
2021-04-26 13:22:53 +0300

    Добавляет карточки с городами и временными зонами

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -1,19 +1,47 @@
 import 'package:analog_clock_and_alarm/screens/components/clock.dart';
+import 'package:analog_clock_and_alarm/screens/components/country_card.dart';
 import 'package:analog_clock_and_alarm/screens/components/time_in_hour_and_minute.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Column(
-        children: [
-          Text("Newport Beach, USA | PST",
-              style: Theme.of(context).textTheme.bodyText1),
-          TimeInHourAndMinute(),
-          Clock()
-        ],
+    return SingleChildScrollView(
+      child: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              Text("Newport Beach, USA | PST",
+                  style: Theme.of(context).textTheme.bodyText1),
+              TimeInHourAndMinute(),
+              // Spacer(),
+              Clock(),
+              // Spacer(),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    CountryCard(
+                      country: "New York, USA",
+                      timeZone: "+3 HRS | EST",
+                      iconSrc: "assets/icons/ic_liberty.svg",
+                      time: "9:20",
+                      periad: "PM",
+                    ),
+                    CountryCard(
+                      country: "New York, USA",
+                      timeZone: "+3 HRS | EST",
+                      iconSrc: "assets/icons/ic_liberty.svg",
+                      time: "9:20",
+                      periad: "PM",
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
